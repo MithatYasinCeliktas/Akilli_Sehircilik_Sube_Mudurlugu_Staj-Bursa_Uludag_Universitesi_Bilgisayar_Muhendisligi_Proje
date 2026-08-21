1c1bb1eddc640be9d0b4d7f6245387f67608d900
MithatYasinCeliktas <mithatyasinc@gmail.com>
2026-08-21 13:12:49 +0300

    Fix CMD chcp 65001 bug in Docker_Baslat.bat

--- Docker_Baslat.bat
+++ Docker_Baslat.bat
@@ -1,22 +1,21 @@
-﻿@echo off
-chcp 65001 >nul
+@echo off
 color 0B
 
 echo ===================================================
-echo   Bursa Faaliyet Raporu - Docker Başlatıcı
+echo   Bursa Faaliyet Raporu - Docker Baslatici
 echo ===================================================
 echo.
-echo [1/2] Eski kapsayıcılar (varsa) durduruluyor...
+echo [1/2] Eski kapsayicilar (varsa) durduruluyor...
 docker-compose down
 
 echo.
-echo [2/2] Tüm sistem sıfırdan derlenip başlatılıyor...
-echo (Bu işlem ilk seferde 3-5 dakika sürebilir)
+echo [2/2] Tum sistem sifirdan derlenip baslatiliyor...
+echo (Bu islem ilk seferde 3-5 dakika surebilir)
 docker-compose up -d --build
 
 echo.
 echo ===================================================
-echo   SİSTEM BAŞARIYLA AYAĞA KALDIRILDI!
+echo   SISTEM BASARIYLA AYAGA KALDIRILDI!
 echo   Frontend: http://localhost:4200
 echo   Backend API: http://localhost:8000/docs
 echo ===================================================
